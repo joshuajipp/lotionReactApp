ce2280e9e7899f57bd45fe5015c35ba369b323a5
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2023-03-24 14:49:08 -0600

    fixed exception bugs

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,31 +1,40 @@
-# add your delete-note function here
 import json
 import boto3
 import requests
 
+
 def handler(event, context):
     dynamodb_resource = boto3.resource("dynamodb")
     table = dynamodb_resource.Table("lotion-30160521")
-    header = event['headers']
-    note_id = event["pathParameters"]["id"]
-
-    req = requests.get(f'https://www.googleapis.com/oauth2/v1/userinfo?access_token={header["token"]}',
-                       headers={
-                           "Authorization": f"Bearer {header['token']}",
-                           "Accept": "application/json"
-                       })
-    if req.status_code == 401:
-        return {
-            "statusCode": 401,
-            "body": json.dumps({"message": "Invalid user"})
-        }
-    
+    header = event["headers"]
+    hash_key = header["email"]
+    sort_key = header["uuid"]
     try:
-        table.delete_item(Key={"id": note_id})
+        req = requests.get(f'https://www.googleapis.com/oauth2/v1/userinfo?access_token={header["token"]}',
+                           headers={
+                               "Authorization": f"Bearer {header['token']}",
+                               "Accept": "application/json"
+                           })
+        if req.status_code == 401:
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"message": "Invalid user"})
+            }
+    except Exception as e:
+        return {
+            "statusCode": 409,
+            "body": json.dumps({"error": str(e)})
+        }
+
+    try:
+        table.delete_item(Key={
+            "email": hash_key,
+            "uuid": sort_key
+        })
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": "note deleted successfully"
+                "message": "deleted note success"
             })
         }
     except Exception as e:
